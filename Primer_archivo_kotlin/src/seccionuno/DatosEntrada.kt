140cd4a2710e75d6c18a9c8a10328cc699d0f398
carlosgijon <carlosgijonfernandez@gmail.com>
2018-08-13 16:54:35 +0200

    nueva subida: Sección 2 Variables

---
+++ Primer_archivo_kotlin/src/seccionuno/DatosEntrada.kt
@@ -0,0 +1,15 @@
+package seccionuno
+
+fun main(args: Array<String>) {
+
+    println("¿Cuál es tu nombre?")
+    val nombre = readLine()
+
+    println("hola $nombre!!")
+
+    println("¿Cuál es tu edad?")
+    val edad : Int = readLine()!!.toInt()
+
+    println("Tienes $edad años!!!")
+
+}
