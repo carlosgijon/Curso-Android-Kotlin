fc31dcc413272b70d3cddcfe96c7afe9252ca2a1
Carlos Gijon <carlosgijonfernandez@gmail.com>
2018-10-22 10:33:22 +0200

    nueva subida: Ejemplos y ejercicios de la sección 3

---
+++ Primer_archivo_kotlin/src/secciontres/OperadoresRelacionales.kt
@@ -0,0 +1,17 @@
+package secciontres
+
+fun main(args: Array<String>) {
+
+    println(8 > 15)
+    println(5 == 5)
+    println(5 != 5)
+
+    val num = 50
+
+    println(num >= 0 && num <=50)  // AND
+    println(num > 100 || num < 60)  // OR
+
+    val num2: Boolean = true
+    println(num2)
+
+}
---
+++ Primer_archivo_kotlin/src/secciontres/SimpleIf.kt
@@ -0,0 +1,12 @@
+package secciontres
+
+fun main(args: Array<String>) {
+
+    println("¿Cual es tu sueldo?")
+    val sueldo : Double = readLine()!!.toDouble()
+
+    if(sueldo > 1000) {
+        println("Debe de pagar impuestos")
+    }
+
+}
